fd2ba609f8e1402d8047b70e4e17c7ee9b6c706d
Fabio Mazza <fab4mazz@gmail.com>
2024-05-22 16:14:51 +0200

    add standard dev to stats

--- src/stats.jl
+++ src/stats.jl
@@ -1,3 +1,5 @@
+vec_mean_std(x) = [mean_and_std(x)]
+
 function find_avg_attack_rates_alldata(simdatas, datatile)
     alldas = DataFrame[] #s=simdatas[1]
     minfs=[]
@@ -12,14 +14,14 @@ function find_avg_attack_rates_alldata(simdatas, datatile)
         push!(alldas,df)
     end
     dff = reduce(vcat,alldas)
-    combine(groupby(dff,:tile_id), :frac_inf => mean => :mean_inf, :rel_att => mean), minfs
+    combine(groupby(dff,:tile_id), :frac_inf => vec_mean_std => [:mean_finf, :finf_std], :rel_att => vec_mean_std => [:rel_att_mean, :rel_att_std]), minfs
 end
 
-function tiles_misinfect_stats_full(misinfstats, tile_data)
+function tiles_misinfect_stats_full(misinfstats, tile_data; people_col=:people)
     mis_stats_shuff = reduce(vcat,misinfstats);
-    mean_misinf_shuff = combine(groupby(mis_stats_shuff,:tile_id), :n_misinf => mean => :mean_n_misinf)
+    mean_misinf_shuff = combine(groupby(mis_stats_shuff,:tile_id), :n_misinf => vec_mean_std => [:mean_n_misinf, :n_misinf_std], )
     mean_misinf_shuff = innerjoin(mean_misinf_shuff, tile_data, on="tile_id")
-    mean_misinf_shuff[!,:frac_misinf_mean] = @. mean_misinf_shuff.mean_n_misinf / mean_misinf_shuff.count
+    mean_misinf_shuff[!,:frac_misinf_mean] = @. mean_misinf_shuff.mean_n_misinf / mean_misinf_shuff[!,people_col]
 
     mean_misinf_shuff
 end
